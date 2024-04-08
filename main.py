19478c80d8f69f0c9a785132d7a68c222d07ff3e
vialaky <vialaky@gmail.com>
2024-04-08 19:22:37 +0300

    Set lxml as parser

--- main.py
+++ main.py
@@ -1,21 +1,14 @@
-import lxml
+# import lxml
 import requests
 from bs4 import BeautifulSoup
 
 
 url = 'https://jobs.dou.ua/vacancies/?category=Python'
-# url = 'https://www.google.com/'
-# soup = BeautifulSoup(lxml, 'html.parser')
-# url = 'https://jobs.dou.ua/'
 
 header = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:32.0) Gecko/20100101 Firefox/32.0',}
 page = requests.get(url, headers=header)
 
-soup = BeautifulSoup(page.text, "html.parser")
+soup = BeautifulSoup(page.text, "lxml")
 
-# print(soup.prettify())
-
-# r = requests.get(url, headers=header)
-# print(r.status_code)
 
 print(soup)
