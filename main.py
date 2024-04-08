acba07ed73f93e5893fd14208d57dd9b24911892
vialaky <vialaky@gmail.com>
2024-04-08 19:51:17 +0300

    Get list of 20 vacancies

--- main.py
+++ main.py
@@ -4,11 +4,17 @@ from bs4 import BeautifulSoup
 
 
 url = 'https://jobs.dou.ua/vacancies/?category=Python'
-
 header = {'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.9; rv:32.0) Gecko/20100101 Firefox/32.0',}
-page = requests.get(url, headers=header)
 
+vacancies = []
+
+page = requests.get(url, headers=header)
 soup = BeautifulSoup(page.text, "lxml")
 
 
-print(soup)
+vacancies = [v for v in soup.find_all('a', class_='vt')]
+
+for vac in vacancies:
+    print(vac)
+
+print(len(vacancies))
